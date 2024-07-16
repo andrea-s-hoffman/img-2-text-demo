624c7919c99eb3a536def54f6fc03040664d1208
andrea-s-hoffman <andrea.s.hoffman09@gmail.com>
2024-07-16 17:30:33 -0400

    filename

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -14,7 +14,7 @@ const ImageUploader = () => {
     const files = fileInputRef.current?.files;
     if (files && files[0]) {
       const newImage = files[0];
-      const storageRef = ref(storage, newImage.name);
+      const storageRef = ref(storage, newImage.name + new Date());
       uploadBytes(storageRef, newImage).then((uploadRes) => {
         getDownloadURL(uploadRes.ref).then((url) => {
           console.log(url);
